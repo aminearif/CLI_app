be6972cb6c0ac85eb307d6a47246eef21915bfa3
aminearif <amine.arif@aigot.com>
2022-03-25 01:01:40 +0100

    wip - CLI config - setup date args

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import argparse
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
+ap = argparse.ArgumentParser(description='Stat About Userbase Growth');
+ap.add_argument('StartDate', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'), help='Please enter the start date in this format : DD-MM-YY')
+ap.add_argument('EndDate', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'), help='Please enter the end date in this format : DD-MM-YY')
+args = ap.parse_args()
+def print_date(startDate, endDate):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {startDate} , {endDate}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_date(args.StartDate, args.EndDate)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
