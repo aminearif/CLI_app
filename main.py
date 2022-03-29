23f12343e316f58a16d4369ecbab87b7373d0e93
aminearif <amine.arif@aigot.com>
2022-03-29 16:56:17 +0100

    feat - plot generated successfully

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import argparse
 import datetime
 import requests
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import functions
 
 ap = argparse.ArgumentParser(description='Stat About Userbase Growth');
@@ -10,22 +13,46 @@ ap.add_argument('EndDate', type=str , help='Please enter the end date in this fo
 args = ap.parse_args()
 
 def print_date(startDate, endDate):
+    # Request user base
     userBase = requests.get("http://sam-user-activity.eu-west-1.elasticbeanstalk.com/")
     jsonUserBase= userBase.json().items()
     dict = {}
+
+    # convert date to timestamp for comparing purposes
     dateStart1 = int(time.mktime(datetime.datetime.strptime(startDate, '%d-%m-%Y').timetuple()))
     dateEnd1 = int(time.mktime(datetime.datetime.strptime(endDate, '%d-%m-%Y').timetuple()))
 
     for key, value in jsonUserBase :
         dateKey = int(time.mktime(datetime.datetime.strptime(key, '%d-%m-%Y').timetuple()))
         if (dateKey in range(dateStart1,dateEnd1+1)):
-            print("dates in range date : ",key , "value : ", value)
+            # print("dates in range date : ",key , "value : ", value)
             valuedict = value
             dict.update({dateKey : valuedict})
         else:
             print("Date is not in range")
 
-    print("************",dict)
+    x = np.array(dict.keys())
+    y = np.array(dict.values())
+
+    # date_time = datetime.datetime.fromtimestamp([*dict])
+    date_time = pd.to_datetime([*dict])
+    data = dict.values()
+
+    DF = pd.DataFrame()
+    DF['value'] = data
+    DF = DF.set_index(date_time)
+    plt.plot(DF)
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+    # print(x, y)
+
+    # plt.plot(x, y)
+    #
+    # plt.xlabel("Dates")
+    # plt.ylabel("Users")
+    #
+    # plt.show()
 
 
 if __name__ == '__main__':
